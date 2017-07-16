fc0ffba5a39218ed7e23e984bba7dc6ccbad9b8e
Graham Ollis <plicease@cpan.org>
2017-07-15 20:34:07 -0400

    Initial commit

---
+++ lib/Alien/Build/Plugin/Download/Git.pm
@@ -0,0 +1,10 @@
+package Alien::Build::Plugin::Download::Git;
+
+use strict;
+use warnings;
+use 5.008001;
+
+# ABSTRACT: Alien::Build plugin to download from git
+# VERSION
+
+1;
---
+++ t/alien_build_plugin_download_git.t
@@ -0,0 +1,6 @@
+use Test2::V0;
+use Alien::Build::Plugin::Download::Git;
+
+ok 1, 'todo';
+
+done_testing
